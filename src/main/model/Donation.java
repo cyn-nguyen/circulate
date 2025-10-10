20567440d89cdebfbb919ab6cbb0c0c921ea8b2b
Cynthia Nguyen <nguyenc2@students.cs.ubc.ca>
2025-10-10 08:13:53 -0700

    Implemented Donation

--- src/main/model/Donation.java
+++ src/main/model/Donation.java
@@ -5,55 +5,65 @@ package model;
  * and status of the item (available, pending pick up, or picked up)
  */
 public class Donation {
+    private static final String AVAILABLE = "available";
+    private static final String PENDING = "pending pick up";
+    private static final String PICKED_UP = "picked up";
+
+    private String name;
+    private int quantity; 
+    private String status;
 
     /*
      * REQUIRES: name has a non-zero length and quantity > 0
      * EFFECTS: constructs a new Donation item with the given name 
      *          and quantity; item's status is available
      */
+    
     public Donation(String name, int quantity) {
-        // stub
-    }
+        this.name = name;
+        this.quantity = quantity;
+        this.status = AVAILABLE;
+        }
 
     /*
      * MODIFIES: this
      * EFFECTS: marks the donation status as available
      */
     public void markAsAvailable() {
-        // stub
-    }
+        this.status = AVAILABLE;
+        }
     
     /*
      * MODIFIES: this
      * EFFECTS: marks the donation status as pending pick up
      */
     public void markAsPending() {
-        // stub
-    }
+        this.status = PENDING;
+        }
 
     /*
      * MODIFIES: this
      * EFFECTS: marks the donation status as picked up
      */
     public void markAsPickedUp() {
-        // stub
-    }
+        this.status = PICKED_UP;
+        }
 
     // getters
     public String getName() {
-        return ""; // stub
+        return this.name;
     }
 
     public int getQuantity() {
-        return -1; // stub
+        return this.quantity;
     }
 
     public String getStatus() {
-        return ""; // stub
+        return this.status;
     }
 
     // setters
-    public void setQuantity(int quantity) {
-        // stub
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
     }
 }
