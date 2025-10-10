de5090f6ca87236d5a38c5afcbb8cda97103bd8f
Cynthia Nguyen <nguyenc2@students.cs.ubc.ca>
2025-10-10 05:23:56 -0700

    Added tests for DonationLog and updated DonationLog specification

--- src/main/model/DonationLog.java
+++ src/main/model/DonationLog.java
@@ -24,11 +24,27 @@ public class DonationLog {
     /*
      * REQUIRES: status is either "available" or "pending pick up" or "picked up"
      * EFFECTS: returns a new donation log that is a subset of the parent donation
-     *          log containing only donation entries with the same availability status
+     *          log containing only donation entries with the same status
      */
-    public DonationLog filterByAvailability(String status) {
+    public DonationLog filterByStatus(String status) {
         return null; // stub
     } 
+
+    /*
+     * EFFECTS: checks if a donation with donationName exists in the donation Log;
+     *          returns true if so, otherwise returns false
+     */
+    public boolean hasDonation(String donationName) {
+        return false;
+    }
+
+    /*
+     * REQUIRES: donation log hasDonation()
+     * EFFECTS: returns the donation entry corresponding to the given donationName
+     */
+    public Donation getDonation(String donationName) {
+        return null;
+    }
 
     // getters
     public int getNumEntries() {
--- src/test/model/DonationLogTest.java
+++ src/test/model/DonationLogTest.java
@@ -1,5 +1,93 @@
 package model;
 
-public class DonationLogTest {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DonationLogTest {
+    private DonationLog testDonationLog;
+    private Donation testDonation1;
+    private Donation testDonation2;
+    private Donation testDonation3;
+
+    @BeforeEach
+    void runBefore() {
+        testDonationLog = new DonationLog();
+        testDonation1 = new Donation("test donation 1", 10);
+        testDonation2 = new Donation("test donation 2", 20);
+        testDonation3 = new Donation("test donation 3", 30);
+    }
+
+    /*
+     * Test constructor and getters
+     */
+    @Test
+    void testConstructor() {
+        assertEquals(0, testDonationLog.getNumEntries());
+    }
+
+    /*
+     * Test adding one donation
+     */
+    @Test
+    void testAddDonation() {
+        testDonationLog.addDonation(testDonation1);
+        assertEquals(1, testDonationLog.getNumEntries());
+    }
+
+    /*
+     * Test adding multiple donations
+     */
+    @Test
+    void testAddDonationMultipleTimes() {
+        testDonationLog.addDonation(testDonation1);
+        testDonationLog.addDonation(testDonation2);
+        testDonationLog.addDonation(testDonation3);
+        assertEquals(3, testDonationLog.getNumEntries());
+    }
+
+    /*
+     * Test filtering donation log by status
+     */
+    @Test
+    void testFilterByStatus() {
+        testDonationLog.addDonation(testDonation1);
+        testDonationLog.addDonation(testDonation2);
+        testDonationLog.addDonation(testDonation3);
+        testDonation2.markAsPending();
+        testDonation3.markAsPickedUp();
+        assertEquals(3, testDonationLog.getNumEntries());
+
+        // test filtering by "available" status
+        DonationLog availableDonationLog = testDonationLog.filterByStatus("available");
+        assertEquals(1, availableDonationLog.getNumEntries());
+
+        // test filtering by "pending pick up" status
+        DonationLog pendingDonationLog = testDonationLog.filterByStatus("pending pick up");
+        assertEquals(1, pendingDonationLog.getNumEntries());
+
+        // test filtering by "picked up" status
+        DonationLog pickedUpDonationLog = testDonationLog.filterByStatus("picked up");
+        assertEquals(1, pickedUpDonationLog.getNumEntries());
+
+        // test original donation log is preserved
+        assertEquals(3, testDonationLog.getNumEntries());
+    }
+
+    @Test
+    void testHasDonation() {
+        testDonationLog.addDonation(testDonation1);
+        assertTrue(testDonationLog.hasDonation("test donation 1"));
+        assertFalse(testDonationLog.hasDonation("test donation 2"));
+    }
+
+    @Test
+    void testGetDonation() {
+        testDonationLog.addDonation(testDonation1);
+        testDonationLog.addDonation(testDonation2);
+        assertEquals(testDonation1, testDonationLog.getDonation("test donation 1"));
+    }
 }
