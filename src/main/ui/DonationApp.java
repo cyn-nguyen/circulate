d7221d8ad5ce1a9cb8211ec8324dd14debaff47f
Cynthia Nguyen <nguyenc2@students.cs.ubc.ca>
2025-10-10 11:09:36 -0700

    Added specification for DonationApp

---
+++ src/main/ui/DonationApp.java
@@ -0,0 +1,76 @@
+package ui;
+
+/*
+ * Donation application that allows the user to add donations
+ * into the donation log, view individual donation entries, and
+ * view and filter the entire donation log
+ */
+public class DonationApp {
+
+    /*
+     * EFFECTS: creates a new instance of the donation application
+     */
+    public DonationApp() {
+        // stub
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: runs the donation appplication
+     */
+    public void runDonationApp() {
+        // stub
+    }
+
+    /*
+     * EFFECTS: displays menu options to the user
+     */
+    public void displayMenu() {
+        // stub
+    }
+
+    /*
+     * EFFECTS: reads user input from the menu
+     */
+    public void processInput() {
+        // stub
+    }
+
+    /*
+     * MODIFIES: donationLog
+     * EFFECTS: adds a donation to the end of the donation log
+     */
+    public void doAddDonation() {
+        // stub
+    }
+
+    /* MODIFIES: Donation
+     * EFFECTS: changes the status of a donation according to the
+     *          user's input
+     */
+    public void changeDonationStatus() {
+        // stub
+    }
+
+    /*
+     * EFFECTS: filters the donation log according to the donation 
+     *          status chosen by the user
+     */
+    public void doFilterByStatus() {
+        // stub
+    }
+
+    /*
+     * EFFECTS: displays the donation log to the user
+     */
+    public void displayDonationLog(DonationLog donationLog) {
+        // stub
+    }
+
+    /*
+     * EFFECTS: displays a line that acts as a divider
+     */
+    public void printDivider() {
+        // stub
+    }
+}
